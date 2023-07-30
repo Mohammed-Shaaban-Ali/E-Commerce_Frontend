776f432e48d85e5303502a48ba2bed677170b6b4
Mohammed Shaaban <ms7500746@gnail.com>
2023-07-30 20:01:34 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,128 @@
 import React from "react";
+import newsletter from "../images/newsletter.png";
+import { Link } from "react-router-dom";
+import { BsGithub, BsInstagram, BsLinkedin, BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl ">
+          <div className="row align-items-center justify-content-around">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-10 align-items-center">
+                <img src={newsletter} alt="newsletter" />
+                <h2 className="mb-0 fs-4 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div className="input-group">
+                <input
+                  type="email"
+                  className="form-control py-1"
+                  placeholder="Your Email Address..."
+                  aria-label="Your Email Address..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text Subscribe" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <footer className="py-3">
+        <div className="container-xxl mb-5">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div className="footer-link">
+                <div className="text-white-50 fs-6">
+                  <span className="mb-2"> Demo Store </span>
+                  <br />
+                  No. 1259 Freedom, New York, 1111
+                  <br />
+                  United States
+                  <br />
+                </div>
+                <a
+                  className="text-white-50 mt-4 d-block mb-2"
+                  href="tel:+91 8264954234"
+                >
+                  (888) 4344 6000-(888) 1338 8193
+                </a>
+                <div className="text-white-50 py-2 mb-1">
+                  ms7500756@gmail.com
+                </div>
+                <div className="social-icons d-flex align-items-center gap-30 mt-4">
+                  <div className="text-white-50">
+                    <BsLinkedin className=" fs-4" />
+                  </div>
+                  <div className="text-white-50">
+                    <BsInstagram className=" fs-4" />
+                  </div>
+                  <div className="text-white-50">
+                    <BsGithub className=" fs-4" />
+                  </div>
+                  <div className="text-white-50">
+                    <BsYoutube className=" fs-4" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white-50 py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white-50 py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white-50 py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white-50 py-2 mb-1">
+                  Terms Of Servlice
+                </Link>
+                <Link className="text-white-50 py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white-50 py-2 mb-1">About US</Link>
+                <Link className="text-white-50 py-2 mb-1">Faq</Link>
+                <Link className="text-white-50 py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white-50 py-2 mb-1">Laptops</Link>
+                <Link className="text-white-50 py-2 mb-1">HeadPhones</Link>
+                <Link className="text-white-50 py-2 mb-1">Tablets</Link>
+                <Link className="text-white-50 py-2 mb-1">Smart Watches</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()} Powered By Develper Mohamed
+                Shaaban
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
