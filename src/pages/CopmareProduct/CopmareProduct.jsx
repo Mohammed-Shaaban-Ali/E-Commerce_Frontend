f5dd5228edf669e6b96b198d6eb5258069731989
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-03 10:15:39 +0300

    compare products

---
+++ src/pages/CopmareProduct/CopmareProduct.jsx
@@ -0,0 +1,403 @@
+import "./CopmareProduct.css";
+import SEO from "../../components/SEO";
+import BreadCrumb from "../../components/BreadCrumb";
+
+import cross from "../../images/cross.svg";
+import watch from "../../images/watch.jpg";
+import { Link } from "react-router-dom";
+
+const CopmareProduct = () => {
+  return (
+    <>
+      <SEO title="Copmare-Product" />
+      <BreadCrumb title="Copmare Product" />
+
+      <div className="compare-products-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 compare-prosuct-card ">
+              <div className="compare-prosuct-croos position-relative">
+                <img
+                  src={cross}
+                  alt="cross"
+                  className="position-absolute img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src={watch} alt="watch" />
+                </div>
+              </div>
+              <div className="product-card-contant">
+                <Link>Honor T1 7.0 1GB RAM 8GB ROM 7 Inch With Wi-Fi+3G </Link>
+                <h6>$ 100.00</h6>
+              </div>
+              <div className="d-flex flex-column">
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Brand:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Type:</h5>
+                  <p>Haveils</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>SKU</h5>
+                  <p>SKU033</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Availability:</h5>
+                  <p>In Stock</p>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Color</h5>
+                  <div className="color-list d-flex gap-2">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+
+                <div className="info d-flex align-items-center justify-content-between">
+                  <h5>Size:</h5>
+                  <div className="size d-flex gap-10">
+                    <p>S</p>
+                    <p>L</p>
+                    <p>XXL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CopmareProduct;
