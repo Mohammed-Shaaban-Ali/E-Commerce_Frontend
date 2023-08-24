b7e91aa7fdc0f672bb02521b327d51bc24a19771
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-24 20:37:41 +0300

    register and login

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import App from "./App";
 import { Provider } from "react-redux";
 import stor from "./redux/stor";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={stor}>
     <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
   </Provider>
 );
