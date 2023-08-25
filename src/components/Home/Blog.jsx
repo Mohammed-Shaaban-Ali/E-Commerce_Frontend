ae8ae9104f8417536c1f50bd575924dd9309fc60
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-25 23:43:45 +0300

    get all blogs in home bahe

--- src/components/Home/Blog.jsx
+++ src/components/Home/Blog.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import BlogCart from "./BlogCart";
 
-const Blog = () => {
+const Blog = ({ blogs }) => {
   return (
     <section className="blog py-5 home-wrapper-2">
       <div className="container-xxl ">
         <h6>Our Lastest News</h6>
         <div className="row">
-          <BlogCart />
-          <BlogCart />
-          <BlogCart />
-          <BlogCart />
+          {blogs?.map((item, index) => (
+            <BlogCart item={item} key={index} />
+          ))}
         </div>
       </div>
     </section>
