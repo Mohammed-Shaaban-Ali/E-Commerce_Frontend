a2f431bef51ae8c44a5e4ebeed8e83f97c27e247
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-26 17:02:15 +0300

    handle special product

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import CardProducts from "./CardProducts";
 
-const Features = () => {
+const Features = ({ products }) => {
   return (
     <section className="products py-5 home-wrapper-2">
       <div className="container-xxl ">
         <h6>Featured Collection</h6>
         <div className="row">
-          <CardProducts />
-          <CardProducts />
-          <CardProducts />
-          <CardProducts />
-          <CardProducts />
-          <CardProducts />
+          {products?.map((product, index) => {
+            if (product?.tags == "featured") {
+              return <CardProducts key={index} product={product} />;
+            }
+          })}
         </div>
       </div>
     </section>
