2162df5bef1c6602fa68389296263f933a99d892
Mohammed Shaaban <ms7500746@gnail.com>
2023-09-02 01:01:39 +0300

    Stor function

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BreadCrumb from "../../components/BreadCrumb";
 import SEO from "../../components/SEO";
@@ -9,10 +9,40 @@ import { getProducts } from "../../redux/slices/productSlice";
 
 const Store = () => {
   const dispatch = useDispatch();
+  const [brands, setBrands] = useState(null);
+  const [categories, setcategories] = useState(null);
+  const [tags, settags] = useState(null);
+
+  const [brand, setBrand] = useState(null);
+  const [category, setcategory] = useState(null);
+  const [tag, settag] = useState(null);
+  const [minPrice, setminPrice] = useState(null);
+  const [maxPrice, setmaxPrice] = useState(null);
+  const [sort, setsort] = useState(null);
   const { products } = useSelector((state) => state.products);
+
   useEffect(() => {
-    dispatch(getProducts());
-  }, []);
+    getproducts();
+  }, [brand, category, tag, minPrice, maxPrice, sort]);
+
+  useEffect(() => {
+    let newBrands = [];
+    let newCategories = [];
+    let newTags = [];
+    for (let i = 0; i < products?.length; i++) {
+      const element = products[i];
+      newBrands.push(element?.brand);
+      newCategories.push(element?.category);
+      newTags.push(element?.tags);
+    }
+    setBrands(newBrands);
+    setcategories(newCategories);
+    settags(newTags);
+  }, [products]);
+
+  const getproducts = () => {
+    dispatch(getProducts({ brand, category, tag, minPrice, maxPrice, sort }));
+  };
   return (
     <>
       <SEO title=" Our Store " />
@@ -20,8 +50,26 @@ const Store = () => {
       <div className="store-wrapper home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
-            <SideBar />
-            <MainStore products={products ? products : []} />
+            <SideBar
+              brands={brands}
+              categories={categories}
+              tags={tags}
+              products={products}
+              minPrice={minPrice}
+              maxPrice={maxPrice}
+              setmaxPrice={setmaxPrice}
+              setminPrice={setminPrice}
+              sort={sort}
+              setsort={setsort}
+              setBrand={setBrand}
+              settag={settag}
+              setcategory={setcategory}
+            />
+            <MainStore
+              sort={sort}
+              setsort={setsort}
+              products={products ? products : []}
+            />
           </div>
         </div>
       </div>
